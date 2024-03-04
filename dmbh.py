8b7a86a8e2a0a8851ddf2d5eecbe74bca8eba9f2
Prem Vijay V <premvpv18@gmail.com>
2024-03-04 20:22:25 +0530

    mass profile evolution plot systematically

--- dmbh.py
+++ dmbh.py
@@ -36,3 +36,84 @@ for i in range(0,Nsteps):
 
 ax.set_ylim(0,1.5)
 
+
+
+#%%
+# Constants
+G = 6.67430e-11  # Gravitational constant
+dt = 1e6  # Time step
+total_time = 1e9  # Total simulation time
+num_shells = 100  # Number of shells
+shell_mass = 1e20  # Initial mass of each shell
+
+
+class SphericalAccretion:
+    def __init__(self, r_min, r_max, num_points, initial_mass_profile):
+        self.r_min = r_min
+        self.r_max = r_max
+        self.num_points = num_points
+        self.initial_mass_profile = initial_mass_profile
+        
+        self.dr = (r_max - r_min) / (num_points - 1)
+        self.r = np.linspace(r_min, r_max, num_points)
+        self.velocities = np.zeros_like(self.r)
+
+    def calculate_gravity(self):
+        gravitational_force = np.zeros_like(self.r)
+        for i in range(1, self.num_points):
+            r_shell = self.r[i]
+            mass_enclosed = self.mass_profile[i]
+            gravitational_force[i] = mass_enclosed / r_shell**2
+        return gravitational_force
+
+    def calculate_mass_profile(self):
+        new_mass_profile = np.zeros_like(self.r)
+        for i in range(self.num_points):
+            new_mass_profile[i] = np.sum(self.initial_mass_profile[self.r <= self.r[i]])
+        return new_mass_profile
+
+    def calculate_velocities(self, dt):
+        for i in range(1, self.num_points):
+            r_shell = self.r[i]
+            force = -self.mass_profile[i] / r_shell**2
+            self.velocities[i] += force * dt
+
+    def update_positions(self, dt):
+        self.r += self.velocities * dt
+
+    def run_simulation(self, total_time, dt):
+        num_steps = int(total_time / dt)
+        for step in range(num_steps):
+            self.mass_profile = self.calculate_mass_profile()
+            gravitational_force = self.calculate_gravity()
+            self.calculate_velocities(dt)
+            self.update_positions(dt)
+
+    def plot_mass_profile(self):
+        plt.plot(self.r, self.mass_profile)
+        plt.xlabel('Radius')
+        plt.ylabel('Enclosed Mass')
+        plt.title('Evolution of Enclosed Mass Profile')
+        plt.show()
+
+# Example usage:
+r_min = 10
+r_max = 20
+num_points = 100
+initial_mass_profile = np.ones(100)
+# initial_mass_profile[0] = 1  # Initial mass at center
+dt = 0.01
+total_time = 2
+
+accretion = SphericalAccretion(r_min, r_max, num_points, initial_mass_profile)
+accretion.run_simulation(total_time, dt)
+accretion.plot_mass_profile()
+accretion.run_simulation(total_time, dt)
+accretion.plot_mass_profile()
+accretion.run_simulation(total_time, dt)
+accretion.plot_mass_profile()
+accretion.run_simulation(total_time, dt)
+accretion.plot_mass_profile()
+
+
+
