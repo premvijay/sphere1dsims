ae049e7ca8ed43d1e4db0f627835354e49641cd6
Prem Vijay V <premvpv18@gmail.com>
2024-03-04 20:18:45 +0530

    bug fix

--- dmbh.py
+++ dmbh.py
@@ -10,25 +10,23 @@ TotMass = 30
 NumShells = 30
 shellMass = TotMass/NumShells
 pos = np.linspace(.05,1,NumShells)
-Hi = 2
+Hi = 2 # hubble flow
 vel = pos*Hi
 col = cm.tab20(np.linspace(0,1,NumShells))
 t=0
-
-def M(r):
-    np.sum(pos<r)
 
 fig, ax = plt.subplots(1,)
 
 TotalTime = 2
 dt=.002
+
 Nsteps = int(TotalTime//dt)
 for i in range(0,Nsteps):
     t+=dt
-    force = -shellMass**2*(pos[:,None]<pos[None]).sum(axis=0)/pos**2/1e1 #
-    # force += .5/pos**3 #angular momentum term
+    accel = -shellMass*(pos[:,None]<pos[None]).sum(axis=0)/pos**2/1e1 # acceleration due to gravity
+    # accel += .5/pos**3 #angular momentum term
     pos += vel*dt
-    vel += force*dt
+    vel += accel*dt
     #For shells crossing through the centre:
     vel *= np.sign(pos)
     pos = np.abs(pos)
