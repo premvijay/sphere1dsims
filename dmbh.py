3b3db05edb88aa768b6448736b61ac988ae73634
Prem Vijay V <premvpv18@gmail.com>
2024-03-04 20:09:19 +0530

    a simple code to simulate sphgerical accretion of sark matter shells starting from hubble flow

---
+++ dmbh.py
@@ -0,0 +1,40 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import pandas as pd
+
+
+#%%
+TotMass = 30
+NumShells = 30
+shellMass = TotMass/NumShells
+pos = np.linspace(.05,1,NumShells)
+Hi = 2
+vel = pos*Hi
+col = cm.tab20(np.linspace(0,1,NumShells))
+t=0
+
+def M(r):
+    np.sum(pos<r)
+
+fig, ax = plt.subplots(1,)
+
+TotalTime = 2
+dt=.002
+Nsteps = int(TotalTime//dt)
+for i in range(0,Nsteps):
+    t+=dt
+    force = -shellMass**2*(pos[:,None]<pos[None]).sum(axis=0)/pos**2/1e1 #
+    # force += .5/pos**3 #angular momentum term
+    pos += vel*dt
+    vel += force*dt
+    #For shells crossing through the centre:
+    vel *= np.sign(pos)
+    pos = np.abs(pos)
+    
+    
+    ax.scatter(pos*0+t,pos,c=col,s=1)
+
+ax.set_ylim(0,1.5)
+
